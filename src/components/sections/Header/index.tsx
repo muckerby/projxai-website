d008e26d074738203964064d7685ae1faa7b045a
muckerby <mcollicoat@gmail.com>
2025-06-26 16:24:26 +1000

    Update index.tsx

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -279,7 +279,7 @@ function ListOfLinks(props) {
                         >
                             <Action
                                 {...link}
-                                className={classNames('whitespace-nowrap', inMobileMenu ? 'w-full' : 'text-lg text-dark font-medium', { // CHANGED: text-dark and font-medium
+                                className={classNames('whitespace-nowrap', inMobileMenu ? 'w-full' : 'text-xl !text-dark font-medium hover:no-underline', { // MODIFIED: text-lg to text-xl, added !text-dark and hover:no-underline
                                     'justify-start py-3': inMobileMenu && link.__metadata.modelName === 'Link'
                                 })}
                                 {...(enableAnnotations && { 'data-sb-field-path': `.${index}` })}
@@ -339,47 +339,11 @@ function LinkWithSubnav(props) {
                     link.labelStyle === 'secondary' ? 'sb-component-link-secondary' : 'sb-component-link-primary',
                     'inline-flex',
                     'items-center',
-                    inMobileMenu ? 'w-full' : 'text-lg',
+                    inMobileMenu ? 'w-full' : 'text-xl !text-dark font-medium hover:no-underline', // MODIFIED: text-lg to text-xl, added !text-dark and hover:no-underline
                     {
                         'group-hover:no-underline hover:no-underline': !inMobileMenu && (link.labelStyle ?? 'primary') === 'primary',
                         'group-hover:text-primary': !inMobileMenu && link.labelStyle === 'secondary'
                     }
                 )}
             >
-                <span {...(fieldPath && { 'data-sb-field-path': '.label' })}>{link.label}</span>
-                <ChevronDownIcon
-                    className={classNames('fill-current', 'shrink-0', 'h-4', 'w-4', isSubNavOpen && 'rotate-180', inMobileMenu ? 'ml-auto' : 'ml-1')}
-                />
-            </button>
-            {(link.links ?? []).length > 0 && (
-                <ul
-                    className={classNames(
-                        'sb-subnav-dropdown',
-                        colors,
-                        inMobileMenu ? 'p-4 space-y-3' : 'absolute top-full left-1/2 -translate-x-1/2 w-44 border-t border-primary shadow-header z-10 px-6 pt-5 pb-6 space-y-4', // CHANGED: left-0 to left-1/2 -translate-x-1/2
-                        isSubNavOpen ? 'block' : 'hidden'
-                    )}
-                    {...(fieldPath && { 'data-sb-field-path': '.links' })}
-                >
-                    <ListOfSubNavLinks links={link.links} hasAnnotations={!!fieldPath} inMobileMenu={inMobileMenu} />
-                </ul>
-            )}
-        </li>
-    );
-}
-
-function ListOfSubNavLinks({ links = [], hasAnnotations, inMobileMenu = false }) {
-    return (
-        <>
-            {links.map((link, index) => (
-                <li key={index}>
-                    <Action
-                        {...link}
-                        className={classNames(inMobileMenu ? 'w-full justify-start' : 'text-lg text-light hover:text-light hover:no-underline')} // CHANGED: added text-light hover:text-light hover:no-underline
-                        {...(hasAnnotations && { 'data-sb-field-path': `.${index}` })}
-                    />
-                </li>
-            ))}
-        </>
-    );
-}
+                <span {...(fieldPath && { 'data-sb-field-path': '.
